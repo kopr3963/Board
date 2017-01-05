cbd0eb52bc957e12f9bb55fcd382d783524bacac
김창회 <kopr3963@naver.com>
2017-01-05 16:21:07 +0900

    d

--- Board/src/com/org/board/Test.java
+++ Board/src/com/org/board/Test.java
@@ -1,9 +1,9 @@
 package com.org.board;
 
 public class Test {
- public static void main(String[] args) {
-	System.out.println("test class");
-	System.out.println("Develop branch");
-	System.out.println("디벨롭 커밋");
-}
+	public static void main(String[] args) {
+		System.out.println("test class");
+		System.out.println("Develop branch");
+		System.out.println("디벨롭 커밋");
+	}
 }
