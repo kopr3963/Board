50e32a9b7de2e905dd6f541279c83904b6f1ef85
김창회 <kopr3963@naver.com>
2017-01-05 15:48:43 +0900

    d

--- Board/src/com/org/board/Test.java
+++ Board/src/com/org/board/Test.java
@@ -1,7 +1,7 @@
 package com.org.board;
 
 public class Test {
- public static void main(String[] args) {
-	System.out.println("test class");
-}
+	public static void main(String[] args) {
+		System.out.println("test class");
+	}
 }
